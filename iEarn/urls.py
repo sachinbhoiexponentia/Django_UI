394d0cfc42b6c40a8006a3ac61d547b8d2ae542e
sachinbhoiexponentia <sachin.bhoi@exponentia.ai>
2023-12-22 16:58:54 +0530

    logout bug fix1 try

--- iEarn/urls.py
+++ iEarn/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('login.urls')),
     path('', include('dashboard.urls')),
     path('', include('app_validation.urls')),
